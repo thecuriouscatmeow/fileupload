8ff5c82e2025bfeba22e91ad186bfbc49d8a52da
Saahil Basak <saahilbasak@thecuriouscat.local>
2024-08-19 10:30:38 +0530

    database connected

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+
+const createDB = new Sequelize('test-db', 'user', 'pass', {
+    dialect: 'sqlite',
+    host: './config/db.sqlite',
+});
+
+const connectDB = () => {
+    createDB.sync().then( () => {
+        console.log('connected to db');
+    })
+    .catch((e) => {
+        console.log('db connection failed', e);
+    })
+}
+
+
+module.exports = { createDB, connectDB }
--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express();
+const { connectDB } = require('./config/db');
 
 
 //middlewares
 app.use(express.json);
 app.use(express.static('content'));
-
+app.use(express.urlencoded({ extends: false }));
 
 const PORT = 1338;
 
 app.listen(PORT => {
     console.log('Server is UP & running on ${PORT}');
+    connectDB();
 });
